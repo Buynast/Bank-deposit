8b04626d1192e3c7972f189e9306d3fea30d3826
anastasiabujnovskaa <buynast@list.ru>
2022-11-23 14:55:23 +0300

    Задание к модулю 12

---
+++ main.py
@@ -0,0 +1,19 @@
+money = int(input('Введите сумму, которую хотите положить под депозит:'))
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+procent = list(per_cent.values())
+
+deposit = [(money*procent[0]/100), (money*procent[1]/100), (money*procent[2]/100), (money*procent[3])/100]
+
+deposit_i = max(deposit)
+
+print('Максимальная сумма, которую вы можете заработать:', deposit_i)
+
+
+
+
+
+
+
+
